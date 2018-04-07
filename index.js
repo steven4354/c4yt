5bf8cff73d7543c2885571c563ed12dab09ea2d2
Steven Li <Steven.Li@walmartlabs.com>
2018-04-07 14:06:35 -0700

    network test

--- index.js
+++ index.js
@@ -2,9 +2,39 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || '3000'
 
+// ----------------------------------------
+// App Locals
+// ----------------------------------------
+app.use((req, res, next) => {
+  res.locals.siteTitle = 'Building and Testing an Express API';
+  next();
+});
+
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan);
+
+// Use morgan middleware with
+// custom format
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morganToolkit());
+}
+
 app.get('/', function (request, response) {
   response.send('Hello Viking!')
 })
+
+let test = require('./routers/test')
+app.use('/test', test)
 
 app.listen(port, function () {
   // This function is run when the app starts up.
