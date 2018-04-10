54439ae133661022adaa2cd518fda492feca3b67
Liam Khong <lkhong@livongo.com>
2018-04-10 08:45:45 -0700

    last minute

--- routers/pots.js
+++ routers/pots.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { exec } = require('child_process');
+const { run } = require('../scripttest.js');
 
 router.get("/", async (req, res, next) => {
   try {
@@ -15,22 +17,40 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:potId", async (req, res, next) => {
-  try {
-    let potId = parseInt(req.params.potId);
+// router.get("/:potId", async (req, res, next) => {
+//   try {
+//     let potId = parseInt(req.params.potId);
 
-    let rawdata = await fs.readFileSync("./database/pots.json");
-    let pots = await JSON.parse(rawdata);
+//     let rawdata = await fs.readFileSync("./database/pots.json");
+//     let pots = await JSON.parse(rawdata);
 
-    res.json(pots[potId]);
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     res.json(pots[potId]);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 router.get("/join", async (req, res, next) => {
   try {
-    await exec('/Users/liamkhong/Documents/cryptoHackathon/bcoin/node_modules/.bin/bwallet-cli send --id=coinbase --value=100 --address=SNNX6LjhSDqDbqi9xn3szsgkcDD8mk4u5Bt', (err, stdout, stderr) => {
+    let receiveAddress;
+    await exec('/Users/liamkhong/Documents/cryptoHackathon/bcoin/node_modules/.bin/bwallet-cli account get default --id=pot0 --token=0af0da7af19fe4a96a9bc60ba3345a594b10c8fae38ddac00bba2b6730875096', (err, stdout, stderr) =>{
+      if (err) {
+        // node couldn't execute the command
+        console.log("err =>", err)
+        return;
+      }
+
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+
+      stdout = JSON.parse(stdout)
+
+      receiveAddress = stdout.receiveAddress;
+
+      console.log("receiveAddress =>", receiveAddress)
+
+      exec(`/Users/liamkhong/Documents/cryptoHackathon/bcoin/node_modules/.bin/bwallet-cli send --id=coinbase --value=1 --address=${receiveAddress}`, (err, stdout, stderr) => {
         if (err) {
           // node couldn't execute the command
           console.log("err =>", err)
@@ -42,43 +62,46 @@ router.get("/join", async (req, res, next) => {
         console.log(`stderr: ${stderr}`);
         res.json(stdout)
     });
+
+    })
+
   } catch (err) {
     console.log(err);
   }
 });
 
-router.get("/join/:potId/:name/:value", async (req, res, next) => {
-  try {
-    let potId = parseInt(req.params.potId);
-    let name = req.params.name;
-    let value = parseInt(req.params.value);
+// router.get("/join/:potId/:name/:value", async (req, res, next) => {
+//   try {
+//     let potId = parseInt(req.params.potId);
+//     let name = req.params.name;
+//     let value = parseInt(req.params.value);
 
-    let rawdata = await fs.readFileSync("./database/pots.json");
-    let pots = await JSON.parse(rawdata);
+//     let rawdata = await fs.readFileSync("./database/pots.json");
+//     let pots = await JSON.parse(rawdata);
 
-    let rawdata = await fs.readFileSync("./database/users.json");
-    let users = await JSON.parse(rawdata);
+//     let rawdata2 = await fs.readFileSync("./database/users.json");
+//     let users = await JSON.parse(rawdata2);
 
-    //add the person to small fishes
-    pots[potId].smallFishes.push({
-      name: name,
-      value: value
-    });
+//     //add the person to small fishes
+//     pots[potId].smallFishes.push({
+//       name: name,
+//       value: value
+//     });
 
-    //deduct their account
-    users[name] -= value;
+//     //deduct their account
+//     users[name] -= value;
 
-    //write to file the new pots
-    let potsStr = await JSON.stringify(pots);
-    await fs.writeFileSync("./database/pots.json", potsStr);
+//     //write to file the new pots
+//     let potsStr = await JSON.stringify(pots);
+//     await fs.writeFileSync("./database/pots.json", potsStr);
 
-    let usersStr = await JSON.stringify(users);
-    await fs.writeFileSync("./database/users.json", usersStr);
+//     let usersStr = await JSON.stringify(users);
+//     await fs.writeFileSync("./database/users.json", usersStr);
 
-    res.redirect("/pots");
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     res.redirect("/pots");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 module.exports = router;
--- scripttest.js
+++ scripttest.js
@@ -24,25 +24,25 @@ let run = async () => {
         //       console.log(`stderr: ${stderr}`);
         //   });
           
-        let returnStdout;
+        // let returnStdout;
 
-        await exec('/Users/liamkhong/Documents/cryptoHackathon/bcoin/node_modules/.bin/bwallet-cli --network=simnet --api-key=fddfc2191ffde1c161e91cda0065ad7e8ecb6c4d837c1885b34e27c1c255d5c3e6338e96603cfb52 get', (err, stdout, stderr) => {
-            if (err) {
-              // node couldn't execute the command
-              console.log("err =>", err)
-              return;
-            }
+        // await exec('/Users/liamkhong/Documents/cryptoHackathon/bcoin/node_modules/.bin/bwallet-cli --network=simnet --api-key=fddfc2191ffde1c161e91cda0065ad7e8ecb6c4d837c1885b34e27c1c255d5c3e6338e96603cfb52 get', (err, stdout, stderr) => {
+        //     if (err) {
+        //       // node couldn't execute the command
+        //       console.log("err =>", err)
+        //       return;
+        //     }
           
-            // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            returnStdout = stdout;
-            console.log("returnStdout in exec =>", returnStdout)
-        });
+        //     // the *entire* stdout and stderr (buffered)
+        //     console.log(`stdout: ${stdout}`);
+        //     console.log(`stderr: ${stderr}`);
+        //     returnStdout = stdout;
+        //     console.log("returnStdout in exec =>", returnStdout)
+        // });
 
-        console.log("returnStdout in run =>", returnStdout)
+        // console.log("returnStdout in run =>", returnStdout)
 
-        return returnStdout
+        // return returnStdout
 
         // await exec('./bwallet-cli --network=simnet --api-key=fddfc2191ffde1c161e91cda0065ad7e8ecb6c4d837c1885b34e27c1c255d5c3e6338e96603cfb52 get', (err, stdout, stderr) => {
         //     if (err) {
